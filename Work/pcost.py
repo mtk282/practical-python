24915ff06d3e0a65bc24327f29225ccda6bab885
Michael King <mtk282@mun.ca>
2021-05-21 09:32:05 -0230

    Solution for Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,17 @@
 # pcost.py
 #
-# Exercise 1.31 - Michael King
+# Exercise 1.32 - Michael King
 
-# Error Handling
+# Using a library function - 'csv' module
+
+import csv
 
 def portfolio_cost(filename):
 
     total_cost = 0.0
 
-    f = open(filename, 'rt')
+    f = open(filename)
+    rows = csv.reader(f)
     headers = next(f)
     for line in f:
         try:
