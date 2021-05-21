df452a38a2cdc7252275bcc960399ff73013f7cf
Michael King <mtk282@mun.ca>
2021-05-21 08:24:52 -0230

    Solution to Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27 - Michael King
+
+# Calculate how much it costs to purchase all of the shares in the portfolio
+
+total_cost = 0.0
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+for line in f:
+    row = line.split(',')
+    #print(row)
+    total_cost = int(row[1]) * float(row[2]) + total_cost
+
+f.close()
+print('Total cost', total_cost)
