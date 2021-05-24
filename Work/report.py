79a3046553d6e7a56d3bea2fc971b2d16269ba41
Michael King <mtk282@mun.ca>
2021-05-24 12:11:43 -0230

    Solution to Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,17 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.4 - Michael King
+
+import csv
+
+def read_portfolio(filename):
+    '''Opens a given portfolio file and reads it into a list of tuples'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
