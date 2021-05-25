069c3ebbdb6fbcfc9e67ddfe08b81e477d38bf4f
Michael King <mtk282@mun.ca>
2021-05-25 10:07:23 -0230

    Solution to Exercise 2.7

--- Work/report.py
+++ Work/report.py
@@ -20,3 +20,39 @@ def read_portfolio(filename):
             portfolio.append(dict_stock)
 
     return portfolio
+
+##Exercise 2.6 - Read the prices into a dictionary
+
+def read_prices(filename):
+    '''Reads prices into a dictionary'''
+    dict = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                dict[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+    return dict
+
+##Exercise 2.7 - Finding out if you can retire
+#Read both dictionaries 
+portfolio = read_portfolio('Data/portfolio.csv')
+dict = read_prices('Data/prices.csv')
+
+# Total cost of the portfolio from portfolio.csv
+total_cost = 0.0
+for s in portfolio:
+    total_cost += s['shares']*s['price']
+
+print('Total cost', total_cost)
+
+# Current value of the portfolio from prices.csv
+total_value = 0.0
+for s in portfolio:
+    total_value += s['shares']*dict[s['name']]
+
+print('Current value', total_value)
+print('Gain', total_value - total_cost)
