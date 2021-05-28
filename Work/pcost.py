7557671d4b0e8e5675d0faceb07656810befc9a6
Michael King <mtk282@mun.ca>
2021-05-28 10:14:05 -0230

    Solution to exercise 3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,35 +1,22 @@
 # pcost.py
 #
-# Exercise 2.15 - Michael King
+# Exercise 3.14 - Michael King
 
-# A practical enumerate() example
+# Using more library imports
 
 import sys
 import csv
+import report
 
 def portfolio_cost(filename):
 
-    total_cost = 0.0
-
-    f = open(filename)
-    rows = csv.reader(f)
-    headers = next(f)
-    #for line in f:
-    for rowno, row in enumerate(rows, start=1):
-        try:
-            #row = line.split(',')
-            #print(row)
-            total_cost = int(row[1]) * float(row[2]) + total_cost
-        except ValueError:
-            #print('Bad row', row)
-            print(f'Row{rowno}: Bad row: {row}')
-
-    return total_cost
+    portfolio = report.read_portfolio(filename)
+    return sum([s['shares']*s['price'] for s in portfolio])
 
 if len(sys.argv) ==2:
     filename = sys.argv[1]
 else:
- filename = 'Data/portfolio.csv'
+ filename = 'Data/portfoliodate.csv'
 
 #f.close()
 cost = portfolio_cost(filename)
